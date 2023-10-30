538748dc903978705d8d6796efee061a9c6e2cf1
chris carroll <ccarroll4812@gmail.com>
2023-10-29 22:16:12 -0400

    meant to do the whole xfce4 desktop

--- termux/setup-termux.sh
+++ termux/setup-termux.sh
@@ -3,7 +3,7 @@ apt -o DPkg::Options::="--force-confnew" -y full-upgrade ;
 # Install termux functionality
 pkg install tur-repo x11-repo -y ;
 # Install the desktop display
-pkg install tigervnc xfce4-session firefox -y ;
+pkg install tigervnc xfce4 firefox -y ;
 # install dev tools
 pkg install emacs code-server openjdk-17 nodejs-lts -y ;
 # other
