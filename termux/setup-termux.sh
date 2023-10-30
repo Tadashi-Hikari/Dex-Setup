1b99fbb23aeef8af471c8c233592f2ae3ead04d4
Chris Carroll <ccarroll4812@gmail.com>
2023-10-29 21:15:02 -0400

    minor update

--- termux/setup-termux.sh
+++ termux/setup-termux.sh
@@ -1,11 +1,13 @@
 pkg update -y
 pkg upgrade -y
-# Install essential functionality
+# Install termux functionality
 pkg install tur-repo x11-repo -y
 # install dev tools
-pkg install emacs git openjdk-17 -y
+pkg install emacs code-server git openjdk-17 nodejs -y
 # Install the desktop display
 pkg install tigervnc xfce4-session firefox -y
+# other
+pkg install proot-distro termux-api yarn
 
 cp start-vnc.sh ~/
 cp clear-vnc.sh ~/
