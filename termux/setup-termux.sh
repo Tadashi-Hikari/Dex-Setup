3e4f441034c8ebd1b44f9631cafbf81a155e994e
chris carroll <ccarroll4812@gmail.com>
2023-10-29 21:47:51 -0400

    fixed

--- termux/setup-termux.sh
+++ termux/setup-termux.sh
@@ -1,13 +1,13 @@
-apt update -y
-apt full-upgrade
+apt update -y ;
+apt -o DPkg::Options::="--force-confnew" -y full-upgrade ;
 # Install termux functionality
-pkg install tur-repo x11-repo -y
+pkg install tur-repo x11-repo -y ;
 # Install the desktop display
-pkg install tigervnc xfce4-session firefox -y
+pkg install tigervnc xfce4-session firefox -y ;
 # install dev tools
-pkg install emacs code-server git openjdk-17 nodejs -y
+pkg install emacs code-server openjdk-17 nodejs-lts -y ;
 # other
-pkg install proot-distro termux-api yarn -y
+pkg install proot-distro termux-api yarn -y ; 
 
 cp start-vnc.sh ~/
 cp clear-vnc.sh ~/
